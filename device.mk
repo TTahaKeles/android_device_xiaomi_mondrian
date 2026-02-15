6c1717970df1eb2570af4810d7e52809d30829ca
TTahaKeles <tanimazsinxxl@gmail.com>
2026-02-15 03:56:50 +0300

    mondrian: vintf: Include Dolby framework compatibility matrix

--- device.mk
+++ device.mk
@@ -49,3 +49,7 @@ PRODUCT_COPY_FILES += \
 # Vibrator
 $(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.xiaomi)
 $(call soong_config_set_bool,qti_vibrator,use_effect_stream,true)
+
+# Dolby compatibility matrix
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    hardware/dolby/dolby_framework_matrix.xml
